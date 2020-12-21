74692ed51f296b1207f5df5f80a6bab33ade7cb1
franken <frank_60@mail.ru>
2020-12-21 17:07:23 +0300

    upgrade bootstrap

---
+++ BlogMVC/BlogMVC/Models/EntityConfiguration/EntityBlog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogMVC.Models.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BlogMVC.Models.EntityConfiguration
+{
+    public class EntityBlog : EntityTypeConfiguration<Blog>
+    {
+        public EntityBlog()
+        {
+            Property(p => p.Title).IsRequired().HasMaxLength(100);
+            Property(p => p.Body).IsRequired().HasMaxLength(3000);
+            Property(p => p.LastEditDateTime).IsRequired();
+        }
+    }
+}
---
+++ BlogMVC/BlogMVC/Models/ProfileAutomapper/BlogProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogMVC.Models.DTO;
+using BlogMVC.Models.Entity;
+using AutoMapper;
+
+
+namespace BlogMVC.Models.ProfileAutomapper
+{
+    public class BlogProfile : Profile
+    {
+        public BlogProfile()
+        {
+            CreateMap<Blog, BlogDto>().ReverseMap();
+        }
+    }
+}
